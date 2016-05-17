8bf88a99f5a17b4d3e017a5922d9bd48db046887
LoopySeo <seogangmin@loopystory.com>
2016-05-18 00:15:12 +0900

    temp working

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('index!');
-  res.render('login', null);
+  res.render('index', null);
 });
 
-router.get('/main', function(req, res, next) {
-  res.render('layout', {view:'main',result:null});
+router.get('/home', function(req, res, next) {
+  res.render('layout', {view:'home/HOME_GUEST',result:null});
+});
+
+router.get('/vote', function(req, res, next){
+  res.render('layout', {view:'idea/IDEA_GUEST_VOTE',result:null});
+});
+
+router.get('/teamchoice', function(req, res, next){
+  res.render('layout', {view:'team/TEAM_GUEST_CHOICE',result:null});
 });
 
 module.exports = router;
